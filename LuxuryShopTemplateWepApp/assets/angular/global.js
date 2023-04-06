0c1621cea49db740cdf639d137e4fc26e70ff158
Vinamilk <nhatnguyen20102002@gmail.com>
2023-04-06 17:08:57 +0700

    add image, load product

--- LuxuryShopTemplateWepApp/assets/angular/global.js
+++ LuxuryShopTemplateWepApp/assets/angular/global.js
@@ -1,4 +1,6 @@
 var current_url = "https://localhost:7099";
+var current_url_admin = "https://localhost:7062";
+var current_img = "E:\\testcode\\Năm 3\\Kì 2\\Lập Trình Web API\\BTL_WEB_API_ShopQuanAo\\Image"; 
 makeScript = function (url) {
 	var script = document.createElement('script');
 	script.setAttribute('src', url);
--- LuxuryShopTemplateWepApp/assets/angular/home.js
+++ LuxuryShopTemplateWepApp/assets/angular/home.js
@@ -7,21 +7,70 @@ app.controller("HomeCtrl", function ($scope, $http) {
     //         url: current_url + '/api/Home/get-moi/10',
     //     }).then(function (response) {			 
     //         $scope.listSanPhamMoi = response.data;
-	// 		makeScript('js/main.js')
+    // 		makeScript('js/main.js')
     //     });
     // };  
-
-    $scope.listCategories;  
-    $scope.LoadCategoryByLanguage = function () { 
-		var key = 'LanguageId';
-		var value = window.location.search.substring(window.location.search.indexOf(key)+key.length+1);		 
+    var key = 'LanguageId';
+    var value = window.location.search.substring(window.location.search.indexOf(key) + key.length + 1);
+    if (value == '') {
+        value = 'vi-VN'
+    }
+    $scope.listCategories;
+    $scope.LoadCategoryByLanguage = function () {
         $http({
-            method: 'GET', 
-            url: current_url + '/api/Categories/getCategoriesByLanguage/'+value,
-        }).then(function (response) { 
+            method: 'GET',
+            url: current_url + '/api/Categories/getCategoriesByLanguage/' + value,
+        }).then(function (response) {
             $scope.listCategories = response.data;
             console.log($scope.listCategories)
-			makeScript('/assets/js/main.js')
         });
-    };  
+    };
+
+    $scope.listProducts;
+    $scope.page = 1;
+    $scope.pageSize = 8;
+    $scope.languageId = value;
+    $scope.LoadProductByLanguage = function () {
+        $http({
+            method: 'POST',
+            data: { page: $scope.page, pageSize: $scope.pageSize,languageId: $scope.languageId },
+            url: current_url + '/api/Product/getProductByLanguage',
+        }).then(function (response) {
+            $scope.listProducts = response.data.data;
+            console.log($scope.listProducts)
+        });
+    };
+
+
+    $scope.listProductsNew;
+    $scope.Quantity = 4;
+    $scope.LoadProductNew = function () {
+        $http({
+            method: 'POST',
+            data: { Quantity: $scope.Quantity,LanguageId: $scope.languageId },
+            url: current_url + '/api/Product/getProductNew',
+        }).then(function (response) {
+            $scope.listProductsNew = response.data;
+            console.log($scope.listProductsNew)
+        });
+    };
+
+    $scope.listProductsBestSeller;
+    $scope.QuantitySeller = 8;
+    $scope.LoadProducBestSeller = function () {
+        $http({
+            method: 'POST',
+            data: { Quantity: $scope.QuantitySeller,LanguageId: $scope.languageId },
+            url: current_url + '/api/Product/getProductBestSeller',
+        }).then(function (response) {
+            $scope.listProductsBestSeller = response.data;
+            console.log($scope.listProductsBestSeller)
+        });
+    };
+
+
+    $scope.LoadCategoryByLanguage();
+    $scope.LoadProductByLanguage();
+    $scope.LoadProductNew();
+    $scope.LoadProducBestSeller();
 });
